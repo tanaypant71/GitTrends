715a02a93d0ff232720123cf9713ffc93da84572
TANAY PANT <tanaypant71@gmail.com>
2023-10-30 10:40:34 +0530

    Searching added using TextWatcher

--- app/src/main/java/com/example/gittrends/MainActivity.kt
+++ app/src/main/java/com/example/gittrends/MainActivity.kt
@@ -2,18 +2,24 @@ package com.example.gittrends
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gittrends.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     //here I am creating a RepoAdapter Instance
     val repoAdapter = RepoAdapter()
+
+    var originalList: List<ItemsItem> = emptyList()
 
     //this is my onCreate Method which is called when the activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,14 +38,40 @@ class MainActivity : AppCompatActivity() {
 
             if (response.isSuccessful) { //checking network request
                 response.body().let { //extract the response
-                    if (it != null) {
-                        repoAdapter.swapData(it.items as List<ItemsItem>)  //updating the adapter with new data
-                    }
+                   originalList = it?.items as List<ItemsItem>
+                    repoAdapter.swapData(originalList)
                 }
             }
             else{   //else showing error message
                 Toast.makeText(this@MainActivity,"Error in Request",Toast.LENGTH_SHORT).show()
             }
         }
+
+        binding.etSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Not needed here
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val query = p0.toString().toLowerCase(Locale.ROOT)
+                val filteredList = originalList.filter {
+                    it.name?.toLowerCase(Locale.ROOT)?.contains(query) == true
+                }
+                repoAdapter.swapData(filteredList)
+
+                if(filteredList.isEmpty()){
+                    Toast.makeText(this@MainActivity, "No data found", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    repoAdapter.swapData(filteredList)
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                //Not needed here
+            }
+
+        })
+
     }
 }
