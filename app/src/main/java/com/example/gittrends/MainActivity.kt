0094a65e3662115888c3cae68b41362dc7206b0d
TANAY PANT <tanaypant71@gmail.com>
2023-10-30 15:15:21 +0530

    Searching comment

--- app/src/main/java/com/example/gittrends/MainActivity.kt
+++ app/src/main/java/com/example/gittrends/MainActivity.kt
@@ -47,6 +47,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        //This part helps in the searching of the
         binding.etSearch.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 //Not needed here
